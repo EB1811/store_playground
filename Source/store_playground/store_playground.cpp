0eee509b800041002931f03308f89e770f0e486d
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2025-02-03 11:46:44 +0000

    init

---
+++ Source/store_playground/store_playground.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "store_playground.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, store_playground, "store_playground");
