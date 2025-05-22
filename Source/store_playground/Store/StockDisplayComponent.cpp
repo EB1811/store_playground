0b4abc0d8161af2d061eb38de0eb225d3314d2ac
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2025-05-22 17:05:16 +0100

    Items data json + sprites

--- Source/store_playground/Store/StockDisplayComponent.cpp
+++ Source/store_playground/Store/StockDisplayComponent.cpp
@@ -13,6 +13,8 @@ void UStockDisplayComponent::BeginPlay() { Super::BeginPlay(); }
 void UStockDisplayComponent::SetDisplaySprite(UPaperFlipbook* ItemSprite) {
   check(ItemSprite);
 
+  // TODO: Set sprite to face the camera.
+
   UPaperFlipbookComponent* Sprite = GetOwner()->FindComponentByClass<UPaperFlipbookComponent>();
   Sprite->SetFlipbook(ItemSprite);
 }
