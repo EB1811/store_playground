d2e7668c341ffbb257bd70ca1e1a602914ca813c
Emmanuils Borovikovs <emmanuilsborovikovs@outlook.com>
2025-05-29 16:33:51 +0100

    Added items data json

--- Source/store_playground/Item/ItemBase.h
+++ Source/store_playground/Item/ItemBase.h
@@ -6,6 +6,7 @@
 #include "ItemDataStructs.h"
 #include "ItemBase.generated.h"
 
+// TODO: Consider BoughtAt price when calculating quantity stacks.
 USTRUCT()
 struct FItemPriceData {
   GENERATED_BODY()
